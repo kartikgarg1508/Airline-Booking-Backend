3ae1a0e4883254705ecf5034d1eafd9e26f530fb
Aditya <aditya.1211y@gmail.com>
2024-01-01 11:44:29 +0530

    Enhancemnt: custom message can be passed as object

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express();
 
 const { ServerConfig, LoggerConfig } = require("./config");
 const apiRoutes = require("./routes");
+const { message } = require("./utils/common/success-response");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,5 +12,7 @@ app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
   console.log(`Server started successfully at Port : ${ServerConfig.PORT}`);
-  LoggerConfig.info("Successfully started server", {});
+  /* NOTE: any { message } property in a meta object provided will automatically be concatenated to any msg already *  provided: For example the below will concatenate ': Test Message' onto 'Successfully started server':
+  */
+  LoggerConfig.info("Successfully started server", {message: ": Test Message"});
 });
